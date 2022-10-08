f8df547a2797daac2bb68f96a8a5addeb6e1bee8
Vinícius André da Veiga <viniciusdaveiga.dev@gmail.com>
2022-10-08 18:19:51 -0300

    REST API for list

--- backend/controllers/listController.js
+++ backend/controllers/listController.js
@@ -1,10 +1,14 @@
 const asyncHandler = require('express-async-handler')
 
+const List = require('../models/listModel')
+
 // @desc    Get list
 // @route   GET /api/list
 // @access  Private
 const getList = asyncHandler(async (req, res) => {
-  res.status(200).json({ message:'Get list' })
+  const list = await List.find()
+
+  res.status(200).json(list)
 })
 
 // @desc    Set list item
@@ -15,21 +19,46 @@ const setListItem = asyncHandler(async (req, res) => {
     res.status(400)
     throw new Error('Please add a text field')
   }
-  res.status(200).json({ message:'Set list item' })
+
+  const listItem = await List.create({
+    text: req.body.text
+  })
+
+  res.status(200).json(listItem)
 })
 
 // @desc    Update list item
 // @route   PUT /api/list/:id
 // @access  Private
 const updateListItem = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: `Update list item ${req.params.id}` })
+  const listItem = await List.findById(req.params.id)
+
+  if(!listItem) {
+    res.status(400)
+    throw new Error('List item not found')
+  }
+
+  const updatedListItem = await List.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+  })
+
+  res.status(200).json(updateListItem)
 })
 
 // @desc    Delete list item
 // @route   DELETE /api/list/:id
 // @access  Private
 const deleteListItem = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: `Delete list item ${req.params.id}` })
+  const listItem = await List.findById(req.params.id)
+
+  if(!listItem) {
+    res.status(400)
+    throw new Error('List item not found')
+  }
+
+  await listItem.remove()
+
+  res.status(200).json({ id: req.params.id })
 })
 
 module.exports = {
